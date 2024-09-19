d0993d40f35edd8cc1158a1327d3ef3b4d36c9b8
Suman Yadav <sumanyadav@Sumans-MacBook-Pro.local>
2024-09-19 00:26:33 -0500

    first commit

---
+++ multipleArguement.js
@@ -0,0 +1,13 @@
+let add = function(a, b, c, d){
+    let result = a+b+c+d;
+    return result;
+}
+let value = add(1, 2, 3, 4);
+console.log(value);
+
+let multiply = function(e, e, f, g){
+    let resultTwo = e*f*g*h;
+    return resultTwo;
+}
+let num = add(1, 3, 5, 7);
+console.log(num);
